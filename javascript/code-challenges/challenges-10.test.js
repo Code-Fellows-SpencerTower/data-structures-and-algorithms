866667637ca49c8d378fe47238d318c334d57e56
SpencerTower <spencerjtower1@gmail.com>
2021-12-11 16:31:45 -0800

    completes challenge 5

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -105,7 +105,11 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  // Solution code here...
+  const newArr = [];
+  hours.forEach((hour, idx) => {
+    newArr.push({ sales: `${data[idx]} cookies`, time: `${hour}` });
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
