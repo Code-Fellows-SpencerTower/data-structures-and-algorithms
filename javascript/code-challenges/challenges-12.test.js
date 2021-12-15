3ac8cfbed69665c8b39e02329da156bf87ee24c8
SpencerTower <spencerjtower1@gmail.com>
2021-12-14 23:07:44 -0800

    edits challenge 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -68,7 +68,10 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  // const regex = /[a-zA-z]+\d+/g;
+  const regex1 = /\w+@[a-zA-z]+\.[a-z]{3}\b/g;
+  // const regex2 = /\w+\.\w+@[a-zA-z]+\.[a-z]{3}\b/g;
+  return regex1.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
